9f13e3630dcd73368d43ac1923923e4b1bf6c3d9
Sharif Uddin <shorif2000@hotmail.com>
2018-02-17 20:58:21 +0000

    Update TollBoothOperator.sol

--- contracts/TollBoothOperator.sol
+++ contracts/TollBoothOperator.sol
@@ -165,16 +165,16 @@ contract TollBoothOperator is Pausable, DepositHolder, MultiplierHolder, RoutePr
         require(msg.sender != entryBooth);
         //@todo exithash
         //@todo secret used
-        uint finalFee = depositedWeis * getMultiplier(vType);//getRoutePrice(entryBooth,msg.sender);
+        uint finalFee = getDeposit() * getMultiplier(vType);//getRoutePrice(entryBooth,msg.sender);
 
-        if(finalFee >= depositedWeis) //if the fee is equal to or higher than the deposit, then the whole deposit is used and no more is asked of the vehicle, now or before any future trip.
+        if(finalFee >= getDeposit()) //if the fee is equal to or higher than the deposit, then the whole deposit is used and no more is asked of the vehicle, now or before any future trip.
         {   
             LogRoadExited(msg.sender,hashSecret(exitSecretClear), finalFee, depositedWeis - finalFee);
             return 1;
         }
-        else if (finalFee < depositedWeis) //if the fee is smaller than the deposit, then the difference is returned to the vehicle.
+        else if (finalFee < getDeposit()) //if the fee is smaller than the deposit, then the difference is returned to the vehicle.
         {
-            LogRoadExited(msg.sender,hashSecret(exitSecretClear), finalFee, depositedWeis - finalFee);
+            LogRoadExited(msg.sender,hashSecret(exitSecretClear), finalFee, getDeposit() - finalFee);
             return 1;
         }
         else //if the fee is not known at the time of exit, i.e. if the fee is 0, the pending payment is recorded, and "base route price required" event is emitted and listened to by the operator's oracle.
@@ -182,7 +182,7 @@ contract TollBoothOperator is Pausable, DepositHolder, MultiplierHolder, RoutePr
             LogPendingPayment(hashSecret(exitSecretClear), entryBooth, msg.sender);
             return 2;
         }
-    }    
+    }      
     /**
      * @param entryBooth the entry booth that has pending payments.
      * @param exitBooth the exit booth that has pending payments.
