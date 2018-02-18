4d5dae5ce2542ebffad216845a6984bf4a05550b
Sharif Uddin <shorif2000@hotmail.com>
2018-02-18 01:02:56 +0000

    Update TollBoothOperator.sol

--- contracts/TollBoothOperator.sol
+++ contracts/TollBoothOperator.sol
@@ -168,7 +168,7 @@ contract TollBoothOperator is Pausable, DepositHolder, MultiplierHolder, RoutePr
         require(msg.sender != entryBooth);
         require(mUsedHash[hashSecret(exitSecretClear)] > 0); //secret does not match a hashed one. && secret has already been reported on exit.
         if(getRoutePrice(entryBooth,msg.sender) == 0){ //if the fee is not known at the time of exit, i.e. if the fee is 0, the pending payment is recorded, and "base route price required" event is emitted and listened to by the operator's oracle.
-            mSecret[entryBooth][msg.sender].secretHashed = hashSecret(exitSecretClear);
+            mSecret[entryBooth][msg.sender].secretHashed = exitSecretClear;
             mSecret[entryBooth][msg.sender].vType = vType;
             mPendingPayments[entryBooth][msg.sender] += 1;
             LogPendingPayment(hashSecret(exitSecretClear), entryBooth, msg.sender);
