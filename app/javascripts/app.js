df0f4698b99f2a779ee661c310ef2d9b57616929
Sharif Uddin <shorif2000@hotmail.com>
2018-03-12 23:47:29 +0000

    enter regulator address

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import Promise from 'bluebird';
 import getWeb3 from '../getWeb3';
+import 'react-bootstrap/dist/react-bootstrap';
 import '../stylesheets/app.css';
 
 class App extends React.Component {
 	constructor(props) {
-		super(props)
-
+		super(props)        
 		this.state = {
-			web3: null
+			web3: null,
+            regulator: '',
+            owner: '',
+			formRErrors: []
 		}
+        this.handleChange = this.handleChange.bind(this);
+    	this.handleSubmit = this.handleSubmit.bind(this);
+        this.instantiateContract = this.instantiateContract.bind(this);
 	}
 
-	instantiateContract = () => {
+	instantiateContract = (regulator_address) => {
     	const truffleContract = require('truffle-contract');
         const regulatorJson = require("../contracts/Regulator.json");
     	const Regulator = truffleContract(regulatorJson);
     	Regulator.setProvider(this.state.web3.currentProvider);
 
         // TODO: build UI to load Regulator at address
-        Regulator.at('0x1a2ba2b1d2777ac7ba497a4dc58019b9569aa630').then((instance) => {
+		let self = this;
+        Regulator.at(regulator_address).then((instance) => {
             const regulatorInstance = instance;
 			Promise.promisifyAll(regulatorInstance, { suffix: "Promise"});
             console.log(regulatorInstance);
@@ -27,7 +34,12 @@ class App extends React.Component {
 			regulatorInstance.getOwner()
 			.then(owner => {
 				console.log('owner', owner);
+				self.setState({owner: owner});
 			});
+        })
+        .catch(() => {
+            console.log('Error locating Regulator');
+			self.setState({formRErrors: ['Error locating Regulator']});
         });
 	}
 
@@ -35,7 +47,6 @@ class App extends React.Component {
 		getWeb3
 		.then(results => {
             let web3 = results.web3;
-console.log(web3);
             Promise.promisifyAll(web3.eth, { suffix: "Promise"});
             Promise.promisifyAll(web3.version, { suffix: "Promise"});
 
@@ -43,22 +54,50 @@ console.log(web3);
 			    web3: web3
 			})
 
-			this.instantiateContract();
 		})
 		.catch(() => {
 			console.log('Error finding web3.')
 		})
 	}
 
+    componentDidCatch(error, errorInfo) {
+    	// Catch errors in any components below and re-render with error message
+    	this.setState({
+      		formRErrors: error,
+      		errorInfo: errorInfo
+   	 	})
+    	// You can also log error messages to an error reporting service here
+  	}
+
+	handleChange(event) {
+    	this.setState({regulator: event.target.value});
+  	}
+
+    handleSubmit(event) {
+    	event.preventDefault();
+		this.instantiateContract(this.state.regulator);
+	}	
+
 	render() {
+		const { formRErrors } = this.state;
+		console.log(this);
 		return (
             <div className="App">
 				<header className="App-header">
 				    <h1 className="App-title">B9Lab Final Exam</h1>
 				</header>
-				<p className="App-body">
-                    -
-				</p>
+                <div className="container-fluid">
+                <form className="form-inline" onSubmit={this.handleSubmit}>
+					{formRErrors.map(error => (
+        				<p key={error}>Error: {error}</p>
+        			))}
+				  <div className="form-group mb-2">
+				    <label htmlFor="regulator_address" className="sr-only">Regulaotr Address</label>
+				    <input type="text" className="form-control" id="regulator_address" placeholder="Regulator Address" value={this.state.regulator} onChange={this.handleChange}/>
+				  </div>
+				  <button type="submit" className="btn btn-primary mb-2">Confirm</button>
+				</form>
+                </div>
             </div>
         );
 	}
