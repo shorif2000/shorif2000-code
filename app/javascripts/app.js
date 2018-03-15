61dc2b591699aa126487de31c6de9e30f41a8ccd
shorif2000 <shorif2000@hotmail.com>
2018-03-15 20:51:22 +0000

    regulator and operator loaded

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -4,103 +4,104 @@ import getWeb3 from '../getWeb3';
 import 'react-bootstrap/dist/react-bootstrap';
 import '../stylesheets/app.css';
 
+let regulatorInstance;
+
 class App extends React.Component {
-	constructor(props) {
-		super(props)        
-		this.state = {
-			web3: null,
-            regulator: '',
-            owner: '',
-			formRErrors: []
-		}
+    constructor(props) {
+        super(props)        
+            this.state = {
+                web3: null,
+                regulator: '',
+                owner: '',
+                formRErrors: []
+            }
         this.handleChange = this.handleChange.bind(this);
-    	this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
         this.instantiateContract = this.instantiateContract.bind(this);
-	}
+    }
 
-	instantiateContract = (regulator_address) => {
-    	const truffleContract = require('truffle-contract');
+    instantiateContract = (regulator_address) => {
+        const truffleContract = require('truffle-contract');
         const regulatorJson = require("../contracts/Regulator.json");
-    	const Regulator = truffleContract(regulatorJson);
-    	Regulator.setProvider(this.state.web3.currentProvider);
+        const Regulator = truffleContract(regulatorJson);
+        Regulator.setProvider(this.state.web3.currentProvider);
 
-        // TODO: build UI to load Regulator at address
-		let self = this;
+        let self = this;
         Regulator.at(regulator_address).then((instance) => {
-            const regulatorInstance = instance;
-			Promise.promisifyAll(regulatorInstance, { suffix: "Promise"});
+            regulatorInstance = instance;
+            Promise.promisifyAll(regulatorInstance, { suffix: "Promise"});
             console.log(regulatorInstance);
 
-			regulatorInstance.getOwner()
-			.then(owner => {
-				console.log('owner', owner);
-				self.setState({owner: owner});
-			});
+            return regulatorInstance.getOwner()
+        })
+        .then(owner => {
+            console.log('owner', owner);
+            self.setState({owner: owner});
         })
         .catch(() => {
             console.log('Error locating Regulator');
-			self.setState({formRErrors: ['Error locating Regulator']});
+            self.setState({formRErrors: ['Error locating Regulator']});
         });
-	}
+    }
 
-	componentWillMount() {
-		getWeb3
-		.then(results => {
-            let web3 = results.web3;
-            Promise.promisifyAll(web3.eth, { suffix: "Promise"});
-            Promise.promisifyAll(web3.version, { suffix: "Promise"});
+    componentWillMount() {
+        getWeb3
+            .then(results => {
+                let web3 = results.web3;
+                Promise.promisifyAll(web3.eth, { suffix: "Promise"});
+                Promise.promisifyAll(web3.version, { suffix: "Promise"});
 
-		    this.setState({
-			    web3: web3
-			})
+                this.setState({
+                    web3: web3
+                })
 
-		})
-		.catch(() => {
-			console.log('Error finding web3.')
-		})
-	}
+            })
+        .catch(() => {
+            console.log('Error finding web3.')
+        })
+    }
 
     componentDidCatch(error, errorInfo) {
-    	// Catch errors in any components below and re-render with error message
-    	this.setState({
-      		formRErrors: error,
-      		errorInfo: errorInfo
-   	 	})
-    	// You can also log error messages to an error reporting service here
-  	}
+        // Catch errors in any components below and re-render with error message
+        this.setState({
+            formRErrors: error,
+        errorInfo: errorInfo
+        })
+        // You can also log error messages to an error reporting service here
+    }
 
-	handleChange(event) {
-    	this.setState({regulator: event.target.value});
-  	}
+    handleChange(event) {
+        this.setState({regulator: event.target.value});
+    }
 
     handleSubmit(event) {
-    	event.preventDefault();
-		this.instantiateContract(this.state.regulator);
-	}	
+        event.preventDefault();
+        this.instantiateContract(this.state.regulator);
+    }	
 
-	render() {
-		const { formRErrors } = this.state;
-		console.log(this);
-		return (
-            <div className="App">
-				<header className="App-header">
-				    <h1 className="App-title">B9Lab Final Exam</h1>
-				</header>
+    render() {
+        const { formRErrors } = this.state;
+        console.log(this);
+        return (
+                <div className="App">
+                <header className="App-header">
+                <h1 className="App-title">B9Lab Final Exam</h1>
+                </header>
                 <div className="container-fluid">
                 <form className="form-inline" onSubmit={this.handleSubmit}>
-					{formRErrors.map(error => (
-        				<p key={error}>Error: {error}</p>
-        			))}
-				  <div className="form-group mb-2">
-				    <label htmlFor="regulator_address" className="sr-only">Regulaotr Address</label>
-				    <input type="text" className="form-control" id="regulator_address" placeholder="Regulator Address" value={this.state.regulator} onChange={this.handleChange}/>
-				  </div>
-				  <button type="submit" className="btn btn-primary mb-2">Confirm</button>
-				</form>
+                {formRErrors.map(error => (
+                        <p key={error}>Error: {error}</p>
+                        ))}
+                <div className="form-group mb-2">
+                <label htmlFor="regulator_address" className="sr-only">Regulaotr Address</label>
+                <input type="text" className="form-control" id="regulator_address" placeholder="Regulator Address" value={this.state.regulator} onChange={this.handleChange}/>
                 </div>
-            </div>
-        );
-	}
+                <button type="submit" className="btn btn-primary mb-2">Confirm</button>
+                </form>
+                </div>
+                </div>
+               );
+    }
 }
 
 export default App;
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -9,13 +9,17 @@ let owner1;
 module.exports = function(deployer, network, accounts) {
    deployer.then(function() {
        owner0 = accounts[0];
+       console.log("owner0: " + owner0);
        return Regulator.new({ from: owner0 });
    }).then(instance => {
        regulator = instance;
+       console.log("regulator address: " + regulator.address);
        owner1 = accounts[1];
+       console.log("owner1: " + owner1);
        return regulator.createNewOperator(owner1, 100, { from: owner0 });
    }).then(txObj => {
        address1 = txObj.logs[1].args.newOperator;
+       console.log("operator: " + address1);
        return TollBoothOperator.at(address1);
    }).then(instance => {
        operator = instance;
