197ba67f4443ff7d7b683702a21f047d11088077
Sharif Uddin <shorif2000@hotmail.com>
2018-03-15 17:09:57 +0000

    removed comments

--- contracts/TollBoothHolder.sol
+++ contracts/TollBoothHolder.sol
@@ -7,29 +7,12 @@ contract TollBoothHolder is Owned, TollBoothHolderI {
     
     mapping (address => bool) private mTollBooth;
     
-	function TollBoothHolder() public {
-	}
-	
-	/**
-     * Event emitted when a toll booth has been added to the TollBoothOperator.
-     * @param sender The account that ran the action.
-     * @param tollBooth The toll booth just added.
-     */
+	function TollBoothHolder() public {}
+
     event LogTollBoothAdded(
         address indexed sender,
         address indexed tollBooth);
         
-    /**
-     * Called by the owner of the TollBoothOperator.
-     *     It should roll back if the caller is not the owner of the contract.
-     *     It should roll back if the argument is already a toll booth.
-     *     It should roll back if the argument is a 0x address.
-     *     When part of TollBoothOperatorI, it should be possible to add toll booths even when
-     *       the contract is paused.
-     * @param tollBooth The address of the toll booth being added.
-     * @return Whether the action was successful.
-     * Emits LogTollBoothAdded
-     */
     function addTollBooth(address tollBooth)
         fromOwner
         public
@@ -41,11 +24,7 @@ contract TollBoothHolder is Owned, TollBoothHolderI {
         LogTollBoothAdded(msg.sender,tollBooth);
         return true;
     }
-    
-    /**
-     * @param tollBooth The address of the toll booth we enquire about.
-     * @return Whether the toll booth is indeed part of the operator.
-     */
+
     function isTollBooth(address tollBooth)
         constant
         public
@@ -53,27 +32,11 @@ contract TollBoothHolder is Owned, TollBoothHolderI {
     {
         return mTollBooth[tollBooth];
     }
-    
-    /**
-     * Event emitted when a toll booth has been removed from the TollBoothOperator.
-     * @param sender The account that ran the action.
-     * @param tollBooth The toll booth just removed.
-     */
+
     event LogTollBoothRemoved(
         address indexed sender,
         address indexed tollBooth);
-        
-    /**
-     * Called by the owner of the TollBoothOperator.
-     *     It should roll back if the caller is not the owner of the contract.
-     *     It should roll back if the argument has already been removed.
-     *     It should roll back if the argument is a 0x address.
-     *     When part of TollBoothOperatorI, it should be possible to remove toll booth even when
-     *       the contract is paused.
-     * @param tollBooth The toll booth to remove.
-     * @return Whether the action was successful.
-     * Emits LogTollBoothRemoved
-     */
+
     function removeTollBooth(address tollBooth)
         fromOwner
         public
