7af6bc07a36415c83930fab03d3d728e0ca98737
Sharif Uddin <shorif2000@hotmail.com>
2018-03-15 17:12:22 +0000

    remove comments

--- contracts/DepositHolder.sol
+++ contracts/DepositHolder.sol
@@ -7,28 +7,14 @@ contract DepositHolder is Owned, DepositHolderI {
     
     uint private deposit;
     
-	function DepositHolder(uint _deposit)
+	function DepositHolder(uint _deposit) public
 	{
 		require(_deposit != 0);
 		deposit = _deposit;
 	}
-	
-	/**
-     * Event emitted when the deposit value has been set.
-     * @param sender The account that ran the action.
-     * @param depositWeis The value of the deposit measured in weis.
-     */
+
     event LogDepositSet(address indexed sender, uint depositWeis);
     
-    /**
-     * Called by the owner of the DepositHolder.
-     *     It should roll back if the caller is not the owner of the contract.
-     *     It should roll back if the argument passed is 0.
-     *     It should roll back if the argument is no different from the current deposit.
-     * @param depositWeis The value of the deposit being set, measure in weis.
-     * @return Whether the action was successful.
-     * Emits LogDepositSet.
-     */
     function setDeposit(uint depositWeis)
         fromOwner
         public
@@ -41,10 +27,6 @@ contract DepositHolder is Owned, DepositHolderI {
         return true;
     }
     
-    /**
-     * @return The base price, then to be multiplied by the multiplier, a given vehicle
-     * needs to deposit to enter the road system.
-     */
     function getDeposit()
         constant
         public
