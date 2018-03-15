7ef9d0f022de45c3fbf4b168d07bea5bc1268848
Sharif Uddin <shorif2000@hotmail.com>
2018-03-15 17:12:53 +0000

    remove comments

--- contracts/MultiplierHolder.sol
+++ contracts/MultiplierHolder.sol
@@ -9,29 +9,12 @@ contract MultiplierHolder is Owned, MultiplierHolderI {
     
 	function MultiplierHolder(){
 	}
-	
-	/**
-     * Event emitted when a new multiplier has been set.
-     * @param sender The account that ran the action.
-     * @param vehicleType The type of vehicle for which the multiplier was set.
-     * @param multiplier The actual multiplier set.
-     */
+
     event LogMultiplierSet(
         address indexed sender,
         uint indexed vehicleType,
         uint multiplier);
-        
-    /**
-     * Called by the owner of the TollBoothOperator.
-     *   Can be used to update a value.
-     *   It should roll back if the vehicle type is 0.
-     *   Setting the multiplier to 0 is equivalent to removing it and is acceptable.
-     *   It should roll back if the same multiplier is already set to the vehicle type.
-     * @param vehicleType The type of the vehicle being set.
-     * @param multiplier The multiplier to use.
-     * @return Whether the action was successful.
-     * Emits LogMultiplierSet.
-     */
+
     function setMultiplier(
             uint vehicleType,
             uint multiplier)
@@ -49,13 +32,7 @@ contract MultiplierHolder is Owned, MultiplierHolderI {
         LogMultiplierSet(msg.sender,vehicleType,multiplier);
         return true;
     }
-    
-    /**
-     * @param vehicleType The type of vehicle whose multiplier we want
-     *     It should accept a vehicle type equal to 0.
-     * @return The multiplier for this vehicle type.
-     *     A 0 value indicates a non-existent multiplier.
-     */
+
     function getMultiplier(uint vehicleType)
         constant
         public
