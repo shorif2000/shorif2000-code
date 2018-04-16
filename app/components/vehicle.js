ab538f476eb7cc4cdd9a775b8a4ade8216539e5a
shorif2000 <shorif2000@hotmail.com>
2018-04-16 23:17:28 +0000

    check if it is already a vehicle

--- app/components/vehicle.js
+++ app/components/vehicle.js
@@ -13,6 +13,7 @@ class Vehicle extends Component {
             valueAddress: '',
             valueVehicle: '1',
             vehicles: [],
+            formRErrors: []
         }
         this.accounts = [];
     }
@@ -39,17 +40,32 @@ class Vehicle extends Component {
         let regulator = this.props.regulator;
         let owner = this.props.owner;
         let self = this;
-        regulator.setVehicleType(this.state.valueAddress, this.state.valueVehicle, { from : owner })
+        regulator.getVehicleType(this.state.valueAddress)
+        .then( vehicleType => {
+            if(vehicleType == 0){
+                return vehicleType; 
+            }
+            self.accounts = self.accounts.filter(function(e) { return e !== self.state.valueAddress });
+            self.passDataBack();
+            let vehicles = self.state.vehicles;
+            vehicles.push({address: self.state.valueAddress, type: vehicleType});
+            self.setState({vehicles, valueAddress: self.accounts[0]});
+            return Promise.reject('This vehicle exists.') ;
+        })
+        .then( vehicleType => {
+            return regulator.setVehicleType(this.state.valueAddress, this.state.valueVehicle, { from : owner });
+        })
         .then( tx => {
-            console.log(tx);
             self.accounts = self.accounts.filter(function(e) { return e !== self.state.valueAddress });
             self.passDataBack();
             let vehicles = self.state.vehicles;
             vehicles.push({address: self.state.valueAddress, type: self.state.valueVehicle});
             self.setState({vehicles, valueAddress: self.accounts[0]});
-            console.log("vehicle set");
-            console.log(self);
-        });    
+        })
+        .catch( (error) => {
+            console.log(error);
+            self.setState({formRErrors: [error]});
+        });
         
     }
 
@@ -60,6 +76,7 @@ class Vehicle extends Component {
     }
 
     render(){
+        const { formRErrors } = this.state;
         let options = this.accounts.map((option, index) => (
             <option key={option} value={option}>
                 {option}
@@ -72,13 +89,15 @@ class Vehicle extends Component {
             </option>
         ));
     
-        console.log(this);
         return(
             <div className="row top-buffer">
                 <h2>Vehicle List</h2>
                 <div className="col-xs-10 col-sm-3 col-md-4">
                 <div className="row-fluid">
                     <form onSubmit={this.handleSubmit} className="form-horizontal">
+                        {formRErrors.map(error => (
+                            <p key={error}>Error: {error}</p>
+                        ))}
                         <div className="form-group">
                             <label className="control-label col-sm-2">Available Addresses:</label>
                             <div className="col-sm-10">
