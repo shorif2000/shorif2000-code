637181b0bc1f004c0015d2681b2e64f4f148c368
Sharif Uddin <shorif2000@hotmail.com>
2018-04-28 19:28:59 +0000

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter , browserHistory } from 'react-router-dom'
 import App from './app';
 
 render(
-        <BrowserRouter history={browserHistory} >
         <App />
-        </BrowserRouter>
         , document.getElementById('root'));
